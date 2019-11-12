34bb318993acc61d9b704c854e5b63c37666a55e
ge25faz <ge25faz@mytum.de>
2019-11-12 17:59:16 +0100

    Create Data function finished (OLI)

--- TestOli.py
+++ TestOli.py
@@ -1,9 +1,8 @@
 from problem import Problem
+import numpy as np
 
-tmp = Problem(5, 1)
+tmp = Problem(5, 3)
 
 print(tmp.n)
 
 tmp.create_data()
-
-
--- problem.py
+++ problem.py
@@ -1,4 +1,5 @@
-import numpy
+import numpy as np
+import  math
 
 class Problem:
 
@@ -28,19 +29,47 @@ class Problem:
         self.n = n
         self.m = m
 
+
     def create_data(self):
-
-        # distance matrix
-        self.w_kl = list()
-        for k in range(self.m):
-            self.w_kl[k] = list()
-            for l in range(self.m):
-                if (k % 2 == 0) == (l % 2 == 0):
-                    self.w_kl[k][l] = math.sqrt((k - l) ** 2)
+        # parameter
+        self.a_i = np.empty([self.n])
+        self.b_i = np.empty([self.n])
+        self.d_i = np.empty([self.n])
+        self.p_i = np.empty([self.n])
+        self.f_ij = np.empty([self.n, self.n])
+        des = 0.8
+        # setting of random values
+        for i in range(self.n):
+            self.a_i[i] = np.random.uniform(1, self.n*70/self.m)
+            self.b_i[i] = self.a_i[i] + np.random.uniform(45,74)
+            self.d_i[i] = des*(self.a_i[i]+self.b_i[i])
+            self.p_i[i] = np.random.uniform(10,14)
+            for j in range(self.n):
+                if (self.a_i[i] < self.a_i[j]):
+                    self.f_ij[i][j] = np.random.random_integers(6, 60)
                 else:
-                    self.w_kl[k][l] = k - 2 + 3 + l - 1
+                    self.f_ij[i][j] = 0
+        # gate distances
+        self.createDistanceMatrix()
+        # print(self.a_i) # Todo delete if not necessary anymore
 
-        print(self.w_kl)
+
+    # distance matrix
+    def createDistanceMatrix(self):
+        self.w_kl = np.empty([self.m,self.m])
+        for k in range(self.m):
+            kk = k +1
+            for l in range(self.m):
+                ll = l+1
+                if (kk % 2 == 0) == (ll % 2 == 0):
+                    self.w_kl[k][l] = math.sqrt(((kk - ll)*0.5) ** 2)
+                elif (kk%2==0):
+                    self.w_kl[k][l] = 3 + math.sqrt(((kk - 2)*0.5) ** 2) + math.sqrt(((ll - 1)*0.5) ** 2)
+                elif (ll%2==0):
+                    self.w_kl[k][l] = 3 + math.sqrt(((ll - 2)*0.5) ** 2) + math.sqrt(((kk - 1)*0.5) ** 2)
+                else:
+                    self.w_kl[k][l] = math.sqrt(((kk - ll) * 0.5) ** 2)
+        # print(self.w_kl) Todo delete if not necessary anymore
 
 
 # subroutines
