43197320e149a7780c892bcd5bc0fcdcd0af5a83
Nicolas Busch <42951229+nico089@users.noreply.github.com>
2019-11-12 12:17:39 +0100

    Created input file

--- problem.py
+++ problem.py
@@ -1,4 +1,5 @@
 class Problem:
+
     def __init__(self,
                  number_of_flights,
                  number_of_gates,
@@ -12,7 +13,47 @@ class Problem:
         self.walking_distance = walking_distance,
         self.number_of_passengers = number_of_passengers
 
+    def __init__(self,
+                 number_of_flights,
+                 number_of_gates):
+        self.number_of_flights = number_of_flights
+        self.number_of_gates = number_of_gates
+        self.flight_parameters = 2,
+        self.walking_distance = 1,
+        self.number_of_passengers = 3
 
-d = Problem(2, 2, [3, 2], 4, 5)
+    def create_data(self, oli):
 
-print(d.number_of_flights)
+# Allgemeine Methoden
+
+    def shift_left(self):
+        return 'hello world'
+
+    def shift_right(self):
+        return 'hello world'
+
+    def shift_interval(self):
+        return 'hello world'
+
+    def attempt_shift_interval(self):
+        return 'hello world'
+
+    def attempt_shift_interval_right(self):
+        return 'hello world'
+
+    def insert(self):
+        return 'hello world'
+
+    def tabu_search(self):
+        return 'hello world'
+
+    def genetic_algorithm(self):
+        return 'hello world'
+
+    def memetic_algorithm(self):
+        return 'hello world'
+
+    def solve(self):
+        return 'hello world'
+
+
