9d96d9bc015a5565d36a9a7461bf3a815a269400
Nicolas Busch <42951229+nico089@users.noreply.github.com>
2019-12-03 17:11:24 +0100

    experiment added

---
+++ tests/experiment.py
@@ -0,0 +1,36 @@
+import pandas as pd
+
+from problem import Problem
+from tabusearch import TabuSearch
+from geneticalgorithm import GeneticAlgorithm
+from memeticalgorithm import MemeticAlgorithm
+from beecolony import BeeColony
+from gurobi import Gurobi
+
+instances = [(12, 3), (12, 3), (12, 4), (12, 4), (13, 4), (13, 4), (14, 4), (14, 4), (15, 4), (15, 4),
+             (20, 5), (20, 5), (22, 5), (22, 5), (24, 6), (24, 6), (26, 6), (26, 6), (28, 6), (28, 6),
+             (50, 10), (50, 10), (60, 10), (60, 12), (80, 12), (80, 15), (100, 15), (100, 20)]
+sizes = ['small'] * 10 + ['medium'] * 10 + ['large'] * 8
+
+results = pd.DataFrame(columns=['Instance', 'Algorithm', 'Size', '#Flights', '#Gates', 'Objective Value', 'Runtime'])
+results.set_index(['Instance', 'Algorithm'], inplace=True)
+
+for index, instance in enumerate(instances, 1):
+
+    prob = Problem(*instance)
+
+    ts = TabuSearch(prob)
+    ts.solve()
+    results.loc[(index, 'Tabu Search'), :] = [sizes[index - 1], *instance, ts.solutions[-1], ts.runtimes[-1]]
+
+    ga = GeneticAlgorithm(prob)
+    ga.solve()
+    results.loc[(index, 'Genetic Algorithm'), :] = [sizes[index - 1], *instance, ga.solutions[-1], ga.runtimes[-1]]
+
+    ma = MemeticAlgorithm(prob)
+    ma.solve()
+    results.loc[(index, 'Memetic Algorithm'), :] = [sizes[index - 1], *instance, ma.solutions[-1], ma.runtimes[-1]]
+
+results.to_csv('results.csv')
+
+
