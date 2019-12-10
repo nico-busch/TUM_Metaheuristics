359bdf2f7f1833d4aac9b00b6fbc5d9e99eaa0c8
ge25faz <ge25faz@mytum.de>
2019-12-10 18:04:29 +0100

    safety push

--- experiments/plot_3.py
+++ experiments/plot_3.py
@@ -42,7 +42,6 @@ paper_ga1_runtime_large = np.array([126, 143, 168, 266, 637, 649, 649, 1323])
 paper_ga2_runtime_large = np.array([147, 141, 294, 227, 568, 863, 1780, 2178])
 
 # read data
-'''
 ts_run_small = data[np.in1d(data.index.get_level_values(1), ['Tabu Search'])][data['Size']=='small'].loc[:,'Runtimes'].apply(tmp)
 ga_run_small = data[np.in1d(data.index.get_level_values(1), ['Genetic Algorithm'])][data['Size']=='small'].loc[:,'Runtimes'].apply(tmp)
 ma_run_small = data[np.in1d(data.index.get_level_values(1), ['Memetic Algorithm'])][data['Size']=='small'].loc[:,'Runtimes'].apply(tmp)
@@ -57,8 +56,9 @@ ts_run_large = data[np.in1d(data.index.get_level_values(1), ['Tabu Search'])][da
 ga_run_large = data[np.in1d(data.index.get_level_values(1), ['Genetic Algorithm'])][data['Size']=='large'].loc[:,'Runtimes'].apply(tmp)
 ma_run_large = data[np.in1d(data.index.get_level_values(1), ['Memetic Algorithm'])][data['Size']=='large'].loc[:,'Runtimes'].apply(tmp)
 bc_run_large = data[np.in1d(data.index.get_level_values(1), ['Bee Colony'])][data['Size']=='large'].loc[:,'Runtimes'].apply(tmp)
-'''
 
+
+'''
 # Version two with objective values
 ts_run_small = data[np.in1d(data.index.get_level_values(1), ['Tabu Search'])][data['Size']=='small'].loc[:,'Objective Values'].apply(tmp)
 ga_run_small = data[np.in1d(data.index.get_level_values(1), ['Genetic Algorithm'])][data['Size']=='small'].loc[:,'Objective Values'].apply(tmp)
@@ -74,10 +74,9 @@ ts_run_large = data[np.in1d(data.index.get_level_values(1), ['Tabu Search'])][da
 ga_run_large = data[np.in1d(data.index.get_level_values(1), ['Genetic Algorithm'])][data['Size']=='large'].loc[:,'Objective Values'].apply(tmp)
 ma_run_large = data[np.in1d(data.index.get_level_values(1), ['Memetic Algorithm'])][data['Size']=='large'].loc[:,'Objective Values'].apply(tmp)
 bc_run_large = data[np.in1d(data.index.get_level_values(1), ['Bee Colony'])][data['Size']=='large'].loc[:,'Objective Values'].apply(tmp)
-
-
-
 '''
+
+
 # plot 3
 fig, ax = plt.subplots()
 
@@ -123,74 +122,4 @@ x = np.linspace(*ax.get_xlim())
 ax.plot(x, x, '--k')
 ax.set_xlim(xmin=0)
 ax.set_ylim(ymin=0)
-plt.show()
-'''
-
-fig, axs = plt.subplots(2, 2)
-
-col = {'Gurobi': 'r',
-       'Tabu Search': 'y',
-       'Genetic Algorithm': 'g',
-       'Memetic Algorithm': 'c',
-       'Bee Colony': 'b'}
-
-axs[0, 0].set_title('Tabu Search')
-axs[0, 0].scatter(ga_run_small, ts_run_small, color=col['Genetic Algorithm'])
-axs[0, 0].scatter(ma_run_small, ts_run_small, color=col['Memetic Algorithm'])
-axs[0, 0].scatter(bc_run_small, ts_run_small, color=col['Bee Colony'])
-axs[0, 0].scatter(ga_run_medium, ts_run_medium, color=col['Genetic Algorithm'])
-axs[0, 0].scatter(ma_run_medium, ts_run_medium, color=col['Memetic Algorithm'])
-axs[0, 0].scatter(bc_run_medium, ts_run_medium, color=col['Bee Colony'])
-axs[0, 0].scatter(ga_run_large, ts_run_large, color=col['Genetic Algorithm'])
-axs[0, 0].scatter(ma_run_large, ts_run_large, color=col['Memetic Algorithm'])
-axs[0, 0].scatter(bc_run_large, ts_run_large, color=col['Bee Colony'])
-
-axs[0, 1].set_title('Genetic')
-axs[0, 1].scatter(ts_run_small, ga_run_small, color=col['Tabu Search'])
-axs[0, 1].scatter(ma_run_small, ga_run_small, color=col['Memetic Algorithm'])
-axs[0, 1].scatter(bc_run_small, ga_run_small, color=col['Bee Colony'])
-axs[0, 1].scatter(ts_run_medium, ga_run_medium, color=col['Tabu Search'])
-axs[0, 1].scatter(ma_run_medium, ga_run_medium, color=col['Memetic Algorithm'])
-axs[0, 1].scatter(bc_run_medium, ga_run_medium, color=col['Bee Colony'])
-axs[0, 1].scatter(ts_run_large, ga_run_large, color=col['Tabu Search'])
-axs[0, 1].scatter(ma_run_large, ga_run_large, color=col['Memetic Algorithm'])
-axs[0, 1].scatter(bc_run_large, ga_run_large, color=col['Bee Colony'])
-
-axs[1, 0].set_title('Memetic')
-axs[1, 0].scatter(ts_run_small, ma_run_small, color=col['Tabu Search'])
-axs[1, 0].scatter(ga_run_small, ma_run_small, color=col['Genetic Algorithm'])
-axs[1, 0].scatter(bc_run_small, ma_run_small, color=col['Bee Colony'])
-axs[1, 0].scatter(ts_run_medium, ma_run_medium, color=col['Tabu Search'])
-axs[1, 0].scatter(ga_run_medium, ma_run_medium, color=col['Genetic Algorithm'])
-axs[1, 0].scatter(bc_run_medium, ma_run_medium, color=col['Bee Colony'])
-axs[1, 0].scatter(ts_run_large, ma_run_large, color=col['Tabu Search'])
-axs[1, 0].scatter(ga_run_large, ma_run_large, color=col['Genetic Algorithm'])
-axs[1, 0].scatter(bc_run_large, ma_run_large, color=col['Bee Colony'])
-
-axs[1, 1].set_title('Bee Colony')
-axs[1, 1].scatter(ts_run_small, bc_run_small, color=col['Tabu Search'])
-axs[1, 1].scatter(ma_run_small, bc_run_small, color=col['Memetic Algorithm'])
-axs[1, 1].scatter(ga_run_small, bc_run_small, color=col['Genetic Algorithm'])
-axs[1, 1].scatter(ts_run_medium, bc_run_medium, color=col['Tabu Search'])
-axs[1, 1].scatter(ma_run_medium, bc_run_medium, color=col['Memetic Algorithm'])
-axs[1, 1].scatter(ga_run_medium, bc_run_medium, color=col['Genetic Algorithm'])
-axs[1, 1].scatter(ts_run_large, bc_run_large, color=col['Tabu Search'])
-axs[1, 1].scatter(ma_run_large, bc_run_large, color=col['Memetic Algorithm'])
-axs[1, 1].scatter(ga_run_large, bc_run_large, color=col['Genetic Algorithm'])
-
-
-
-for i in range(2):
-    for j in range(2):
-        #axs[i, j].set_yscale('log')
-        #axs[i, j].set_xscale('log')
-        x = np.linspace(*axs[i, j].get_xlim())
-        axs[i, j].plot(x, x, '--k')
-        #plt.axis('square')
-
-        #x = np.linspace(*axs[i, j].get_xlim())
-        #axs[i, j].plot(axs[i, j].get_xlim(), axs[i, j].get_ylim(), '--k')
-        #axs[i, j].set_aspect('equal', adjustable='box')
-        #plt.axis('square')
-
 plt.show()
